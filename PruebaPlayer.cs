3f85de3d799d46ec6562fea321dd5f8de5f267ee
Rivadia <rivadia@gmail.com>
2017-10-07 02:26:59 -0500

    Create PruebaPlayer.cs
    Esta es una prueba para GitHub

---
+++ PruebaPlayer.cs
@@ -0,0 +1,28 @@
+Using System;
+Using System.Collections;
+Using UnityEngine;
+
+public class Player : MonoBehavior{
+  private int vidaPlayer;
+  private float velocidadPlayer;
+  
+  
+  public int vidaPlayer{
+    
+    get{
+      return vidaPlayer;
+    
+    }
+    set
+    {
+    
+      this.player = value;
+    
+    }
+  
+  
+  
+  }
+
+
+}
